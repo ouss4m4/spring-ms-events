b3c61b04149b1b4fc620c367fe3063d71f4b265a
o.baaziz <o.baaziz@pharmainvest.dz>
2021-12-28 17:08:23 +0100

    gateway setup

---
+++ api-gateway-server/src/test/java/com/springapi/ms/apigatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.ms.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
